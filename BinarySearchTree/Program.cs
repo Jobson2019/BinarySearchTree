cf32bc425f12d8896d05ccd65fa49b0648ee3609
Kevin Jobson <jobsonk2877@gmail.com>
2019-10-15 12:15:49 -0500

    final product

---
+++ BinarySearchTree/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinarySearchTree
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Node node = new Node();
+            SearchTree searchTree = new SearchTree();
+
+
+            searchTree.Add(15);
+            searchTree.Add(12);
+            searchTree.Add(50);
+            searchTree.Add(61);
+            searchTree.Add(33);
+            searchTree.Add(67);
+            searchTree.Add(11);
+            searchTree.Add(34);
+
+        }
+    }
+}
---
+++ BinarySearchTree/SearchTree.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinarySearchTree
+{
+    class SearchTree
+    {
+
+
+
+
+      
+        public Node root;
+        Node main;
+        public void BinarySearchTree()
+        {
+            root = null;
+        }
+        public void Add(int value)
+        {
+            Node newNode = new Node();
+            newNode.value = value;
+            if (root == null)
+            {
+                root = newNode;
+                main = root;
+            }
+            else
+            {
+
+                if (value > main.value)
+                {
+                    if (main.right == null)
+                    {
+                        main.right = newNode;
+                    }
+                    else
+                    {
+                        main = main.right;
+                        Add(value);
+                    }
+                }
+                else if (value < main.value)
+                {
+                    if (main.left == null)
+                    {
+                        main.left = newNode;
+                    }
+                    else
+                    {
+                        main = main.left;
+                        Add(value);
+                    }
+                }
+                main = root;
+            }
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+        public bool SearchNodes(Node node, int searchValue)
+        {
+
+            if (searchValue > node.value)
+            {
+                if (node.right == null)
+                {
+                    return false;
+                }
+                else
+                {
+                    //needs to go right
+                    return SearchNodes(node.right, searchValue);
+                }
+
+            }
+            else if (searchValue < node.value)
+            {
+                if (node.left == null)
+                {
+                    return false;
+                }
+                else
+                {
+                    //needs to go left
+                    return SearchNodes(node.left, searchValue);
+                }
+            }
+
+            else
+            {
+                return true;
+            }
+
+
+
+
+
+
+
+        }
+    }
+}
